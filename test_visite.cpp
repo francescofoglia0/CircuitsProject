352c46e155778b72006be670441488674712108f
francescofoglia0 <francifoglia182@gmail.com>
2026-06-18 15:35:31 +0000

    tolto dijkstra

--- test_visite.cpp
+++ test_visite.cpp
@@ -58,15 +58,6 @@ int main(void)
             cout << "Errore\n";
             return EXIT_FAILURE;
         }
-
-
-        // test algoritmo di dijkstra
-        auto albero_dijkstra = dijkstra(G, 1);
-        // l'albero dei cammini minimi da una sorgente copre tutto il grafo (se connesso)
-        if(albero_dijkstra.all_edges().size() != numero_nodi) {
-            cout << "Errore\n";
-            return EXIT_FAILURE;
-        }
     }
 
     cout << "Tutto giusto\n";
